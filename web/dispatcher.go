3296e376597a203fc34eb8ff7d0cceae8fb6578c
heloise <37453877+heloise-viegas@users.noreply.github.com>
2023-09-07 16:17:19 +0530

    Update dispatcher.go

--- web/dispatcher.go
+++ web/dispatcher.go
@@ -29,7 +29,7 @@ type forwarder struct {
 func (f *forwarder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	addrs, err := net.LookupHost(f.host)
 	if err != nil {
-		log.Println("Error", err)
+		log.Println("Error:", err)
 		http.Error(w, err.Error(), 500)
 		return
 	}
